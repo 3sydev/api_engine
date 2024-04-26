5ea059575377d20de996a7971fc885ce2fa588b1
michael-mala <michael@3sy.dev>
2024-04-26 12:38:31 +0200

    Built version "1.1.4"

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -2,7 +2,7 @@ export type ApiTypes = {
     [key: string]: string;
 };
 export type IgnoreGlobalParam = 'request' | 'retry' | 'retryCondition' | 'stackTraceLogExtraParams' | 'requestInterceptor' | 'responseInterceptor';
-export type RequestInterceptor = (endpoint: Endpoint) => Endpoint;
+export type RequestInterceptor = (endpoint: Endpoint) => Endpoint | Promise<Endpoint>;
 export type ResponseInterceptor = (response: CallResponse) => object | Promise<object> | undefined | null | void;
 export type Endpoint = {
     baseUrl?: string;
